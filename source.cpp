fe8bff0a1b3a02e7d2289de6559f6be9e7a6430f
lucasneitorestudio <159574448+lucasneitorestudio@users.noreply.github.com>
2024-06-08 01:37:53 -0400

    funcion delete, creada por Ariany

--- source.cpp
+++ source.cpp
@@ -10,7 +10,7 @@ map<string, set<string>> events;
 
 //Funcion por Lucas, add_event
 void add_event(const string& date, const string& event);
-
+bool delete_event(const string& date, const string& event);
 
 int main(){
 
@@ -24,4 +24,15 @@ int main(){
 //Funcion por Lucas, add_event
 void add_event(const string& date, const string& event) {
     events[date].insert(event);
+}
+
+bool delete_event(const string& date, const string& event){
+    if(events.count(date) && events[date].count(event)){
+        events[date].erase(event);
+        if(events[date].empty()){
+            events.erase(date);
+        }
+        return true;
+    }
+    return false;
 }
